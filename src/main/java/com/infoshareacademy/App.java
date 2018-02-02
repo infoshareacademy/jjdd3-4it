333e8250e7fa5b92e6397b09f643c5fbc6ba32e3
Michał Bodzek <michal.bodzek@wp.pl>
2018-02-02 16:57:30 +0100

    Initial solution for reading CSV file.

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,5 +1,7 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.model.ImportStock;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println("Hello World!");
+        ImportStock importStock = new ImportStock();
+        importStock.readFromFile();
     }
 }
--- src/main/java/com/infoshareacademy/model/ImportStock.java
+++ src/main/java/com/infoshareacademy/model/ImportStock.java
@@ -1,4 +1,47 @@
 package com.infoshareacademy.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ImportStock {
+    String sourceFile = "src/main/resources/bitcoin.csv";
+    
+    File file = new File(sourceFile);
+
+    List<String> date = new ArrayList<>();
+    List<String> txVolumeUSD = new ArrayList<>();
+    List<String> txCount = new ArrayList<>();
+    List<String> marketCupUSD = new ArrayList<>();
+    List<String> priceUSD = new ArrayList<>();
+    List<String> exchangeVolumeUSD = new ArrayList<>();
+    List<String> generatedCoins = new ArrayList<>();
+    List<String> fees = new ArrayList<>();
+
+    public void readFromFile() {
+        try (Scanner inputStream = new Scanner(file)) {
+            if (inputStream.hasNext()) {
+                String inputData = inputStream.next();
+            }
+            while (inputStream.hasNext()) {
+                String inputData = inputStream.next();
+                String[] stringsArray = inputData.split(",");
+                date.add(stringsArray[0]);
+                txVolumeUSD.add(stringsArray[1]);
+                txCount.add(stringsArray[2]);
+                marketCupUSD.add(stringsArray[3]);
+                priceUSD.add(stringsArray[4]);
+                exchangeVolumeUSD.add(stringsArray[5]);
+                generatedCoins.add(stringsArray[6]);
+                fees.add(stringsArray[7]);
+            }
+        } catch (FileNotFoundException fnfExc) {
+            System.out.println("Invalid file name or wrong path to file!!!");
+            System.out.println(fnfExc);
+        }
+
+    }
+
 }
