bb005dca77696e3579a61b8580f05e2aef3b18a1
Szymon Mielewczyk <mielewczyk.szymon@gmail.com>
2018-02-12 15:45:59 +0100

    JJDD4IT-27-change regex form to LocalDate in DateService
    Signed-off-by: Szymon Mielewczyk <mielewczyk.szymon@gmail.com>

--- src/main/java/com/infoshareacademy/tools/DateService.java
+++ src/main/java/com/infoshareacademy/tools/DateService.java
@@ -1,44 +1,32 @@
 package com.infoshareacademy.tools;
 
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateService {
-    public static final String DATE_REGEXP = "(201[0-9])-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])";
-    public static final String MENU_REGEXP = "[0-9]";
-    private Pattern datePattern = Pattern.compile(DATE_REGEXP);
-    private Pattern menuPattern = Pattern.compile(MENU_REGEXP);
 
-    public boolean isDateExists(String date) {
-        Matcher matcher = datePattern.matcher(date);
-        if (matcher.matches()) {
-            return isDateInRange(matcher);
-        }
-        return false;
+    public static final String DATE_FORRMAT = "yyyy-MM-dd";
+
+    LocalDate dateCorrectFormat(String input) throws DateTimeParseException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(DATE_FORRMAT);
+        return LocalDate.parse(input, dtf);
     }
 
-    private boolean isDateInRange(Matcher matcher) {
-        int year = Integer.parseInt((matcher.group(1)));
-        String month = matcher.group(2);
-        String day = matcher.group(3);
-
-        if ((month.equals("04") || month.equals("06") || month.equals("09") || month.equals("11")) && day.equals("31")) {
-            return false;
-        } else if (month.equals("02") && (day.equals("30") || day.equals("31") || (year % 4 == 0 && day.equals("29")))) {
-            return false;
-        } else {
-            return true;
-        }
+    public LocalDate getDateFromUser() {
+        LocalDate result = null;
+        boolean flag = false;
+        do {
+            try {
+                result = dateCorrectFormat(InputReaderTool.read());
+                System.out.println(result);
+                flag = true;
+            } catch (DateTimeParseException e) {
+                System.out.println("plese write correct format " + DATE_FORRMAT);
+            }
+        } while (flag == false);
+        return result;
     }
-
-    private boolean isMenuNumberInRange(String menuNumber) {
-        Matcher matcher = menuPattern.matcher(menuNumber);
-        return matcher.matches();
-    }
-
-
-
 
 
 }
