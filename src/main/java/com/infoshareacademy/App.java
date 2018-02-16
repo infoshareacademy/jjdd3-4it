67b410527a95143290362c668dd380fc20d66a4b
jacekklebeko <35174184+jacekklebeko@users.noreply.github.com>
2018-02-16 16:48:50 +0100

    [JJDD4IT-19-17] math calculation, search operation
    * continue
    
    * Now all its working in Math things

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,20 +1,38 @@
 package com.infoshareacademy;
 
-import com.infoshareacademy.controller.StockSorter;
+import com.infoshareacademy.controller.MathStock;
+import com.infoshareacademy.controller.SearchStock;
+import com.infoshareacademy.model.ImportStock;
+import com.infoshareacademy.tools.StockFileReaderService;
 import com.infoshareacademy.view.DisplayMenu;
+
+import java.time.LocalDate;
 
 
 public class App {
     public static void main(String[] args) {
 
+
+        MathStock mathStock = new MathStock();
+        SearchStock searchStock = new SearchStock();
+        LocalDate start = StockFileReaderService.readFile().get(1).getDate();
+        LocalDate end = StockFileReaderService.readFile().get(53).getDate();
+
+        searchStock.streamList(StockFileReaderService.readFile(), start, end);
+        searchStock.printPriceMax(searchStock.streamList(StockFileReaderService.readFile(), start, end));
+        searchStock.printPriceMin(searchStock.streamList(StockFileReaderService.readFile(), start, end));
+        System.out.println(mathStock.averragePriceForRange(searchStock.streamList(StockFileReaderService.readFile(), start, end)));
+        System.out.println(mathStock.medianPriceForRange(searchStock.streamList(StockFileReaderService.readFile(), start, end)));
+
+
         DisplayMenu newMenu = new DisplayMenu();
 
 
-        if (newMenu.hasIncorrectLists()) {
-            return;
-        }
-        newMenu.menuControl();
-
-
+//        if (newMenu.hasIncorrectLists()) {
+//            return;
+//        }
+//        newMenu.menuControl();
+//
     }
+
 }
