27c7d48a26f9083664ef96fdd3cd5fc600738cab
Szymon Mielewczyk <mielewczyk.szymon@gmail.com>
2018-03-02 16:58:38 +0100

    [JJDD4IT-34] add slf4j dependencies in pom.xml, initialize LOG in StockSorter
    Signed-off-by: Szymon Mielewczyk <mielewczyk.szymon@gmail.com>

--- src/main/java/com/infoshareacademy/controller/StockSorter.java
+++ src/main/java/com/infoshareacademy/controller/StockSorter.java
@@ -2,12 +2,17 @@ package com.infoshareacademy.controller;
 
 import com.infoshareacademy.model.InputData;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StockSorter {
+
+    public static final Logger LOG = LoggerFactory.getLogger(StockSorter.class);
 
     public static final Comparator<InputData> byPrice = (d1, d2) -> Double.compare(d1.getPrice(), d2.getPrice());
     public static final Comparator<InputData> byDate = (l1, l2) -> l1.getDate().compareTo(l2.getDate());
