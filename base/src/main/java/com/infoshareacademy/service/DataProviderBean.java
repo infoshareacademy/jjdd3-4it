5e06a41cd8f572d713390e25014644a837a16eaf
Andrea Aquari <aquariandrea@gmail.com>
2018-03-07 16:13:38 +0100

    [JJDD4IT-39] WebServlets - Implement comments by OP MN

--- base/src/main/java/com/infoshareacademy/service/DataProviderBean.java
+++ base/src/main/java/com/infoshareacademy/service/DataProviderBean.java
@@ -10,55 +10,12 @@ import java.util.Scanner;
 @RequestScoped
 public class DataProviderBean {
 
-    private final String pathCurrency = "currencies";
-    private final String pathOperations = "operations";
-    private final String pathGreeting = "greetingmenu";
-    private List<String> operationsList = getFromFile(getPathOperations());
-    private List<String> currencyList = getFromFile(getPathCurrency());
-    private List<String> greetingList = getFromFile(getPathGreeting());
-
-    public String getPathCurrency() {
-        return pathCurrency;
-    }
-
-    public String getPathOperations() {
-        return pathOperations;
-    }
-
-    public String getPathGreeting() {
-        return pathGreeting;
-    }
-
-    public List<String> getOperationsList() {
-        return operationsList;
-    }
-
-    public void setOperationsList(List<String> operationsList) {
-        this.operationsList = operationsList;
-    }
-
-    public List<String> getCurrencyList() {
-        return currencyList;
-    }
-
-    public void setCurrencyList(List<String> currencyList) {
-        this.currencyList = currencyList;
-    }
-
-    public List<String> getGreetingList() {
-        return greetingList;
-    }
-
-    public void setGreetingList(List<String> greetingList) {
-        this.greetingList = greetingList;
-    }
-
-    public List getFromFile(String pathCurrency) {
+    public List getFromFile(String path) {
 
         Scanner newScanner = null;
 
         try {
-            newScanner = new Scanner(new File(pathCurrency));
+            newScanner = new Scanner(new File(path));
         } catch (FileNotFoundException e) {
             System.out.println("Problems with origin file");
             return new ArrayList();
