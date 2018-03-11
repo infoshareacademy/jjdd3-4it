299f1ce38868c74b5f9c2951f8e5780cc95ce415
Szymon Mielewczyk <mielewczyk.szymon@gmail.com>
2018-03-11 22:58:35 +0100

    [JJDD4UT-34] update loggers -DateService -CountingFuntionBean StockCalculationsListsServlet
    Signed-off-by: Szymon Mielewczyk <mielewczyk.szymon@gmail.com>

--- web/src/main/java/com/infoshareacademy/cdi/CountingFunctionsBean.java
+++ web/src/main/java/com/infoshareacademy/cdi/CountingFunctionsBean.java
@@ -5,6 +5,8 @@ import com.infoshareacademy.controller.SearchStock;
 import com.infoshareacademy.controller.StockSorter;
 import com.infoshareacademy.model.InputData;
 import com.infoshareacademy.tools.StockFileReaderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ejb.Stateless;
 import java.time.LocalDate;
@@ -12,8 +14,9 @@ import java.util.List;
 
 @Stateless
 public class CountingFunctionsBean {
-
+    private static Logger LOG = LoggerFactory.getLogger(CountingFunctionsBean.class);
     private StockFileReaderService stockFileReaderService = new StockFileReaderService();
+
 
     public List<InputData> readFileBean(String cryptoFile) {
         return stockFileReaderService.readFile(cryptoFile);
@@ -21,33 +24,30 @@ public class CountingFunctionsBean {
 
     private StockSorter stockSorter = new StockSorter();
 
-    public List<InputData> sortDataByBean(String cryptoFile, LocalDate startDate, LocalDate endDate){
-        return stockSorter.sortDataBy(readFileBean(cryptoFile),stockSorter.byPrice, startDate, endDate);
+    public List<InputData> sortDataByBean(String cryptoFile, LocalDate startDate, LocalDate endDate) {
+        LOG.info("choosing cryptoFile");
+        return stockSorter.sortDataBy(readFileBean(cryptoFile), stockSorter.byPrice, startDate, endDate);
     }
 
     private SearchStock searchStock = new SearchStock();
 
-    public InputData printMaxPriceBean(String cryptoFile, LocalDate startDate, LocalDate endDate){
+    public InputData printMaxPriceBean(String cryptoFile, LocalDate startDate, LocalDate endDate) {
         return searchStock.printPriceMax(sortDataByBean(cryptoFile, startDate, endDate));
     }
 
-    public InputData printMinPriceBean(String cryptoFile, LocalDate startDate, LocalDate endDate){
+    public InputData printMinPriceBean(String cryptoFile, LocalDate startDate, LocalDate endDate) {
         return searchStock.printPriceMin(sortDataByBean(cryptoFile, startDate, endDate));
     }
 
     private MathStock mathStock = new MathStock();
 
-    public double medianPriceForRangeBean(String cryptoFile, LocalDate startDate, LocalDate endDate){
+    public double medianPriceForRangeBean(String cryptoFile, LocalDate startDate, LocalDate endDate) {
         return mathStock.medianPriceForRange(sortDataByBean(cryptoFile, startDate, endDate));
     }
 
-    public double avaragePriceForRangeBean(String cryptoFile, LocalDate startDate, LocalDate endDate){
+    public double avaragePriceForRangeBean(String cryptoFile, LocalDate startDate, LocalDate endDate) {
         return mathStock.averagePriceForRange(sortDataByBean(cryptoFile, startDate, endDate));
     }
-
-
-
-
 
 
 }
--- web/src/main/java/com/infoshareacademy/servlet/OperationServlet.java
+++ web/src/main/java/com/infoshareacademy/servlet/OperationServlet.java
@@ -36,7 +36,7 @@ public class OperationServlet extends HttpServlet {
 
         try {
             template.process(dataModel, response.getWriter());
-        } catch(TemplateException e) {
+        } catch (TemplateException e) {
             e.printStackTrace();
         }
     }
