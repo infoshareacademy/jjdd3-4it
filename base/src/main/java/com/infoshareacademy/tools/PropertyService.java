70c3f64fb12bcdce4a5f0586029a7f0336d1827f
Michał Bodzek <michal.bodzek@wp.pl>
2018-03-12 15:23:42 +0100

    [JJDD4IT-65] adding Logging part - logging in ImportStock and PropertyService classes

--- base/src/main/java/com/infoshareacademy/tools/PropertyService.java
+++ base/src/main/java/com/infoshareacademy/tools/PropertyService.java
@@ -1,10 +1,14 @@
 package com.infoshareacademy.tools;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyService {
+    private static Logger LOG = LoggerFactory.getLogger(PropertyService.class);
     private static final String CURRENCY = "currency";
     private static final String DATE_FORMAT = "dateFormat";
     private final String FILE;
@@ -12,11 +16,13 @@ public class PropertyService {
     private Properties properties = new Properties();
 
     public PropertyService() {
+        LOG.info("PropertyService class -> constructor ");
         FILE = "fourIt.properties";
         try {
             loadProperties();
         }catch (IOException ioe){
             System.out.println("File not found!");
+            LOG.error("Chosen data was not found in this FILE {} ", FILE);
         }
     }
 
@@ -25,10 +31,12 @@ public class PropertyService {
     }
 
     public String getCurrency() {
+        LOG.info("User retrieves {} using getCurrency", CURRENCY);
         return getValue(CURRENCY);
     }
 
     public String getDateFormat() {
+        LOG.info("User retrieves {} using getDateFormat", DATE_FORMAT);
         return getValue(DATE_FORMAT);
     }
 
