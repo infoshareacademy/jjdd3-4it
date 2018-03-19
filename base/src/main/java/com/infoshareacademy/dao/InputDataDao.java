735b6b5daf463242f44824af66dc67bf10295e97
Szymon Mielewczyk <mielewczyk.szymon@gmail.com>
2018-03-19 15:33:03 +0100

    [JJDD4IT-42] add logger to InputDataDao

--- base/src/main/java/com/infoshareacademy/dao/InputDataDao.java
+++ base/src/main/java/com/infoshareacademy/dao/InputDataDao.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy.dao;
 
 import com.infoshareacademy.model.InputData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -15,11 +17,14 @@ public class InputDataDao {
     @PersistenceContext
     private EntityManager entityManager;
 
+    private static final Logger LOG = LoggerFactory.getLogger(InputDataDao.class);
+
     public void save(InputData inputData) {
         Query query = entityManager.createQuery("SELECT s FROM InputData s WHERE s.currency=? AND s.date=?");
         query.setParameter(1, inputData.getCurrency());
         query.setParameter(2, inputData.getDate());
         if (query.getResultList().isEmpty()) {
+            LOG.info("Data exist in database");
             entityManager.persist(inputData);
         }
     }
